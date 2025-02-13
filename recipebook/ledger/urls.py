4574aa8c94f38a7cffc0d4d4588326d85996f6c0
Mady <youngmadeleineh@gmail.com>
2025-02-13 22:01:40 +0800

    minor code clean up

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from .views import index, recipe_list, recipe_1, recipe_2
 
 urlpatterns = [
@@ -7,7 +6,6 @@ urlpatterns = [
     path('recipes/list', recipe_list, name='recipes/list'),
     path('recipe/1', recipe_1, name='recipe/1'),
     path('recipe/2', recipe_2, name='recipe/2'),
-
 ]
 
 app_name = "ledger"
