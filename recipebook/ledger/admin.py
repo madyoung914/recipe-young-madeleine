45235475e62c0714b85575b2b3ad47697747ae50
Mady <youngmadeleineh@gmail.com>
2025-02-27 20:10:33 +0800

    did the bonus: admin panels

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient
 
-class IngredientAdmin(admin.ModelAdmin):
-    model = Ingredient
-
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    inlines = [RecipeIngredientInline, ]
 
-class RecipeIngredientAdmin(admin.ModelAdmin):
-    model = RecipeIngredient
-
-admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
