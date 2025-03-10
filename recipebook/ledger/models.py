be1e0b37dcb9866a02b1f816880ca6e1f7cb4a35
Mady <youngmadeleineh@gmail.com>
2025-03-10 17:08:32 +0800

    added the rest of the features

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -12,6 +12,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField(default='Unknown', max_length=50, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,13 @@
 from .models import Recipe
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin,ListView):
     model = Recipe
     template_name = 'recipe_list.html'
 
-class RecipeDetailView(DetailView):
+
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
